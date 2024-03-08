54522b12cec22f2903752d1c6c4cee837c0c536e
Ofir Slavuzki <Ofirsl1993@Gmail.com>
2024-03-08 17:25:43 +0200

    second commit

---
+++ src/lesson_01/FirstRun.java
@@ -0,0 +1,10 @@
+package lesson_01;
+
+public class FirstRun {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ofir");
+        System.out.print("Hello, Ofir");
+        System.out.print("Hello, Ofir");
+    }
+
+}
---
+++ src/lesson_01/Variabels.java
@@ -0,0 +1,38 @@
+package lesson_01;
+
+public class Variabels {
+
+    public static void main(String[] args) {
+
+        byte age = 35;
+        short count;
+        count = 1000;
+
+        System.out.println(age);
+        System.out.println(count);
+        System.out.println(60);
+
+//        age = 128;  byte -> | -128 --- 127 |
+        System.out.println(age);
+
+        int myAge = 35;
+        long myLongNumber = 1L;
+
+        System.out.println("myLongNumber = " + myLongNumber);
+        System.out.println("myAge = " + myAge);
+
+        float myFloat = 35.6f;
+        double myDouble = 456.9876;
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+        System.out.println("myFalse = " + myFalse);
+        System.out.println("myDouble = " + myDouble);
+
+        String myName = "Alex Alex Alex Alex Alex 15 true";
+        System.out.println("myName = " + myName);
+
+        char myChar = ' ';
+        char myChar2 = 'c';
+    }
+}
