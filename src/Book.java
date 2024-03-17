4d02eb3bd0a25a399fba3318200235ef8aa08e04
Ofir Slavuzki <Ofirsl1993@Gmail.com>
2024-03-17 19:54:03 +0200

    commit

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book{
+private String name;
+private int price;
+private int pagesAmount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPagesAmount() {
+        return pagesAmount;
+    }
+
+    public void setPagesAmount(int pagesAmount) {
+        this.pagesAmount = pagesAmount;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
